0b0aa45fdd5a13b14b5324d7613ae3fab36a6009
Randy Ehret <rehret@frontlineed.com>
2017-12-01 14:56:19 -0600

    Clean up day-1/nodejs/reverse-captcha

--- solutions/day-1/nodejs/reverse-captcha.js
+++ solutions/day-1/nodejs/reverse-captcha.js
@@ -1,32 +1,35 @@
 const { Readable } = require('stream');
 const { EventEmitter } = require('events');
 
+class TupleStream extends Readable {
+    constructor(options) {
+        super(options);
+        this._source = options.input;
+        this._index = 0;
+        this._intervalFn = options.intervalFn;
+    }
+
+    _read() {
+        const tupleTarget = this._intervalFn(new Number(this._index), JSON.parse(JSON.stringify(this._source))) % this._source.length;
+        this.push(JSON.stringify([this._source[this._index], this._source[tupleTarget]]));
+
+        this._index++;
+
+        if (this._index === this._source.length) {
+            this.push(null);
+        }
+    }
+}
+
 module.exports.ReverseCaptcha = class ReverseCaptcha {
     constructor(inputStr, intervalFn) {
         let sum = 0;
         const internalEvents = new EventEmitter();
         const publicEvents = new EventEmitter();
-
-        class TupleStream extends Readable {
-            constructor(options) {
-                super(options);
-                this._source = options.input;
-                this._index = 0;
-            }
-
-            _read() {
-                const nextIndex = intervalFn(new Number(this._index), JSON.parse(JSON.stringify(this._source))) % this._source.length;
-                this.push(JSON.stringify([this._source[this._index], this._source[nextIndex]]));
-
-                this._index++;
-
-                if (this._index === this._source.length) {
-                    this.push(null);
-                }
-            }
-        }
-
-        const stream = new TupleStream({ input: inputStr.split('') });
+        const stream = new TupleStream({
+            input: inputStr.split(''),
+            intervalFn: intervalFn
+        });
 
         internalEvents.on('match', val => {
             sum += val;
