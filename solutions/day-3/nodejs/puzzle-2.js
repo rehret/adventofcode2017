e8f4f24210ff416082eedba0d179d1e9adff7e46
Randy Ehret <randyehret@gmail.com>
2017-12-03 10:49:50 -0600

    Add puzzle and solution for day-3/puzzle-2

---
+++ solutions/day-3/nodejs/puzzle-2.js
@@ -0,0 +1,53 @@
+const fs = require('fs');
+const path = require('path');
+
+const input = parseInt(fs.readFileSync(path.resolve(__dirname, '../input.txt'), 'utf-8'));
+
+class SequenceValue {
+    constructor(x, y, val) {
+        this.x = x;
+        this.y = y;
+        this.val = val;
+    }
+
+    static getValueAtCoord(x, y, sequence) {
+        return sequence.filter(s => {
+            return s.x >= x - 1 &&
+                s.x <= x + 1 &&
+                s.y >= y - 1 &&
+                s.y <= y + 1;
+        }).reduce((sum, seq) => sum + seq.val, 0);
+    }
+}
+
+const sequence = [];
+sequence.push(new SequenceValue(0, 0, 1));
+let ring = 1;
+let ringCount = 8;
+let ringPosition = 0;
+let x = 1;
+let y = 0;
+while(sequence[sequence.length - 1].val <= input) {
+    sequence.push(new SequenceValue(x, y, SequenceValue.getValueAtCoord(x, y, sequence)));
+
+    // get next coordinate
+    ringPosition++;
+    const sideLength = parseInt(ringCount / 4);
+    const sideKey = (ringPosition + 1) / sideLength;
+    if (sideKey > 0 && sideKey <= 1) {
+        y++;
+    } else if (sideKey > 1 && sideKey <= 2) {
+        x--;
+    } else if (sideKey > 2 && sideKey <= 3) {
+        y--;
+    } else {
+        x++;
+    }
+
+    if (ringPosition === ringCount) {
+        ringCount += 8;
+        ringPosition = 0;
+    }
+
+}
+console.log(sequence[sequence.length - 1].val);
