fbcba7d74230fd8d74ad1e64dfb34d410eb49dda
Randy Ehret <rehret@frontlineed.com>
2017-12-04 07:58:45 -0600

    Add solution for day-4 puzzle-2

---
+++ solutions/day-4/nodejs/puzzle-2.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+const path = require('path');
+const { Passphrase } = require('./passphrase');
+
+const input = fs.readFileSync(path.resolve(__dirname, '../input.txt'), 'utf-8');
+
+function isAnagram(word1, word2) {
+    const word1Letters = word1.split('').sort((a, b) => a.localeCompare(b));
+    const word2Letters = word2.split('').sort((a, b) => a.localeCompare(b));
+
+    return word1Letters.join('') === word2Letters.join('');
+}
+
+const validPassphrases = Passphrase.getValidPassphrases({
+    input: input,
+    comparator: (searchWord, matchWord, searchIndex, matchIndex, array) => {
+        return !isAnagram(searchWord, matchWord) || searchIndex === matchIndex;
+    }
+});
+
+console.log(validPassphrases.length);
