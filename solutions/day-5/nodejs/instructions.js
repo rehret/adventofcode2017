b7e6e36fa637ca8acfe9875f1bb020a10324ba13
Randy Ehret <rehret@frontlineed.com>
2017-12-05 08:02:19 -0600

    Refactor day-5 puzzle-1

---
+++ solutions/day-5/nodejs/instructions.js
@@ -0,0 +1,22 @@
+module.exports.Instructions = class Instructions {
+    static Process(input, offsetModifier = null) {
+        if (typeof input === 'object') {
+            const options = input;
+            input = options.input;
+            offsetModifier = options.offsetModifier;
+        }
+
+        let index = 0;
+        let moves = 0;
+
+        while (index >= 0 && index < input.length) {
+            const oldIndex = index;
+            const oldValue = input[index];
+            index += input[index];
+            input[oldIndex] = offsetModifier(oldIndex, oldValue, input);
+            moves++;
+        }
+
+        return moves;
+    }
+};
--- solutions/day-5/nodejs/puzzle-1.js
+++ solutions/day-5/nodejs/puzzle-1.js
@@ -3,17 +3,13 @@
 
 const fs = require('fs');
 const path = require('path');
+const { Instructions } = require('./instructions');
 
 const input = fs.readFileSync(path.resolve(__dirname, '../input.txt')).toString().split(/\n/).map(val => parseInt(val));
 
-let index = 0;
-let moves = 0;
-
-while (index >= 0 && index < input.length) {
-    const oldIndex = index;
-    index += input[index];
-    input[oldIndex] = input[oldIndex] + 1;
-    moves++;
-}
+const moves = Instructions.Process({
+    input: input,
+    offsetModifier: (index, val, arr) => val + 1
+});
 
 console.log(moves);
