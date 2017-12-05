5dc66d8d4ee9ba705707bc6ac1c54817d28b4902
Randy Ehret <rehret@frontlineed.com>
2017-12-05 08:06:40 -0600

    Add solution for day-5 puzzle-2

---
+++ solutions/day-5/nodejs/puzzle-2.js
@@ -0,0 +1,20 @@
+#!/usr/bin/env node
+"use strict";
+
+const fs = require('fs');
+const path = require('path');
+const { Instructions } = require('./instructions');
+
+const input = fs.readFileSync(path.resolve(__dirname, '../input.txt')).toString().split(/\n/).map(val => parseInt(val));
+
+const moves = Instructions.Process({
+    input: input,
+    offsetModifier: (index, val, arr) => {
+        if (val >= 3) {
+            return -1;
+        }
+        return 1;
+    }
+});
+
+console.log(moves);
