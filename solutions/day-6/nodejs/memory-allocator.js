ff557f522aeb5c090130b5d1f730c8229ba57646
Randy Ehret <rehret@frontlineed.com>
2017-12-06 07:56:25 -0600

    Add solution for day-6 puzzle-1

---
+++ solutions/day-6/nodejs/memory-allocator.js
@@ -0,0 +1,38 @@
+"use strict";
+
+module.exports.MemoryAllocator = class MemoryAllocator {
+    static CatchInfiniteLoop(input) {
+        if (typeof input === 'object' && !Array.isArray(input)) {
+            const options = input;
+            input = options.input;
+        }
+
+        const memoryBankSnapshots = [];
+        let cycles = 0;
+
+        while (!configurationSeenBefore(input, memoryBankSnapshots)) {
+            memoryBankSnapshots.push(JSON.parse(JSON.stringify(input)));
+
+            const maxIndex = input.reduce((maxIndex, val, index, arr) => val > arr[maxIndex] ? index : maxIndex, 0);
+            const incrementForAll = (input[maxIndex] / input.length) | 0;
+            const incrementRemainder = input[maxIndex] % input.length;
+
+            input[maxIndex] = 0;
+            input = input.map(val => val + incrementForAll);
+            for (let i = 0; i < incrementRemainder; i++) {
+                input[(maxIndex + i + 1) % input.length]++;
+            }
+
+            cycles++;
+        }
+        return cycles;
+    }
+}
+
+function configurationSeenBefore(currentConfig, pastConfigs) {
+    return pastConfigs.some(pastConfig => {
+        return pastConfig.every((val, index) => {
+            return val === currentConfig[index];
+        });
+    });
+}
---
+++ solutions/day-6/nodejs/puzzle-1.js
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+"use strict";
+
+const fs = require('fs');
+const path = require('path');
+const { MemoryAllocator } = require('./memory-allocator');
+
+const input = fs.readFileSync(path.resolve(__dirname, '../input.txt')).toString().split(/\s+/).map(val => parseInt(val));
+
+console.log(MemoryAllocator.CatchInfiniteLoop(input));
