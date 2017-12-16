e31126f5b130f69cde9e131bdc495035ad9ff767
Randy Ehret <randyehret@gmail.com>
2017-12-16 14:30:06 -0600

    Add non-working solution for day-16 puzzle-2

--- solutions/day-16/nodejs/dance-parser.js
+++ solutions/day-16/nodejs/dance-parser.js
@@ -2,43 +2,98 @@
 
 module.exports.DanceParser = class DanceParser {
     /**
-     * @param {string} input
+     * @param {string | Move[]} input
+     * @param {string[]} arr
+     * @returns {string[]}
      */
-    static GetPositionsAfterDance(input) {
-        /** @type {string[]} */
-        let arr = [];
-        for (let i = 0; i < 16; i++) {
-            arr.push(String.fromCharCode(i + 97));
-        }
+    static GetPositionsAfterOneSet(input, arr = null) {
+        arr = arr || generateStartingArray();
 
-        const moves = input.split(/,/);
-
-        const spinRegex = /s(\d+)+/;
-        const swapRegex = /x(\d+)\/(\d+)/;
-        const partnerRegex = /p(\w+)\/(\w+)/;
+        const moves = Array.isArray(input) ? input : parse(input);
 
         for(let move of moves) {
-            if (spinRegex.test(move)) {
-                const matches = move.match(spinRegex);
-                arr = arr.splice(arr.length - parseInt(matches[1])).concat(arr);
-            } else if (swapRegex.test(move)) {
-                const matches = move.match(swapRegex);
-                const index1 = parseInt(matches[1]);
-                const index2 = parseInt(matches[2]);
-                swap(arr, index1, index2);
-            } else if (partnerRegex.test(move)) {
-                const matches = move.match(partnerRegex);
-                const index1 = arr.indexOf(matches[1]);
-                const index2 = arr.indexOf(matches[2]);
-                swap(arr, index1, index2);
-            } else {
-                throw new Error('Unrecognized move');
+            if (move.type === 'spin') {
+                arr = arr.splice(arr.length - parseInt(move.param1)).concat(arr);
+            } else if (move.type === 'swap') {
+                swap(arr, parseInt(move.param1), parseInt(move.param2));
+            } else if (move.type === 'partner') {
+                swap(arr, arr.indexOf(move.param1), arr.indexOf(move.param2));
             }
+        }
+        return arr;
+    }
+
+    /**
+     * @param {string} input
+     * @returns {string[]}
+     */
+    static GetPositionsAfterFullDance(input) {
+        let arr = generateStartingArray();
+        const moves = parse(input);
+
+        for (let i = 0; i < 1000; i++) {
+            arr = DanceParser.GetPositionsAfterOneSet(moves, arr);
         }
 
         return arr;
     }
 };
+
+class Move {
+    /**
+     *
+     * @param {('spin' | 'swap' | 'partner')} type
+     * @param {string} param1
+     * @param {string} param2
+     */
+    constructor(type, param1, param2 = null) {
+        this.type = type;
+        this.param1 = param1;
+        this.param2 = param2;
+    }
+}
+
+/**
+ * @param {string} input
+ * @returns {Move[]}
+ */
+function parse(input) {
+    /** @type {Move[]} */
+    const moves = [];
+    const inputArr = input.split(/,/);
+
+    const spinRegex = /s(\d+)+/;
+    const swapRegex = /x(\d+)\/(\d+)/;
+    const partnerRegex = /p(\w+)\/(\w+)/;
+
+    for (let line of inputArr) {
+        if (spinRegex.test(line)) {
+            const matches = line.match(spinRegex);
+            moves.push(new Move('spin', matches[1]));
+        } else if (swapRegex.test(line)) {
+            const matches = line.match(swapRegex);
+            moves.push(new Move('swap', matches[1], matches[2]));
+        } else if (partnerRegex.test(line)) {
+            const matches = line.match(partnerRegex);
+            moves.push(new Move('partner', matches[1], matches[2]));
+        } else {
+            throw new Error('Unrecognized move');
+        }
+    }
+
+    return moves;
+}
+
+/**
+ * @returns {string[]}
+ */
+function generateStartingArray() {
+    let arr = [];
+    for (let i = 0; i < 16; i++) {
+        arr.push(getCharacterFromIndex(i));
+    }
+    return arr;
+}
 
 /**
  * @param {any[]} arr
@@ -49,4 +104,12 @@ function swap(arr, index1, index2) {
     const temp = arr[index1];
     arr[index1] = arr[index2];
     arr[index2] = temp;
+}
+
+/**
+ * @param {number} index
+ * @returns {string}
+ */
+function getCharacterFromIndex(index) {
+    return String.fromCharCode(index + 97);
 }
--- solutions/day-16/nodejs/puzzle-2.js
+++ solutions/day-16/nodejs/puzzle-2.js
@@ -7,6 +7,6 @@ const { DanceParser } = require('./dance-parser');
 
 const input = fs.readFileSync(path.resolve(__dirname, '../input.txt'), 'utf-8');
 
-const arr = DanceParser.GetPositionsAfterOneSet(input);
+const arr = DanceParser.GetPositionsAfterFullDance(input);
 
 console.log(arr.join(''));
