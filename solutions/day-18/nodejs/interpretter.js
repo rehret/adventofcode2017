b03f9babffc3642ca37fcce05f7fcf5dd6e2254d
Randy Ehret <rehret@frontlineed.com>
2017-12-19 09:36:46 -0600

    Refactor solution for day-18 puzzle-1 to help solve puzzle-2

--- solutions/day-18/nodejs/interpretter.js
+++ solutions/day-18/nodejs/interpretter.js
@@ -2,36 +2,46 @@
 
 const { EventEmitter } = require('events');
 
-const events = new EventEmitter();
 const soundEventName = 'sound';
 const recoverEventName = 'recover';
 
-module.exports.Interpretter = class Interpretter {
+/**
+ * @typedef {Object.<string, typeof Instruction>} InstructionMapping
+ * @property {typeof Instruction} snd
+ * @property {typeof Instruction} set
+ * @property {typeof Instruction} add
+ * @property {typeof Instruction} mul
+ * @property {typeof Instruction} mod
+ * @property {typeof Instruction} rcv
+ * @property {typeof Instruction} jgz
+ */
+
+class Interpretter {
     /**
      * @param {string} input
+     * @param {InstructionMapping} instructionMapping
      * @param {(sound: number) => void} recoverCallback
      */
-    static Execute(input, recoverCallback) {
-        /** @type {Register[]} */
-        const registers = [];
-        const instructions = parse(input);
+    static Execute(input, instructionMapping, recoverCallback) {
+        const state = new ProgramState();
+        state.instructions = parse(input, instructionMapping);
+
         let lastSound = null;
-        let instructionPointer = 0;
         let soundRecovered = false;
 
-        events.on(soundEventName, sound => {
+        state.events.on(soundEventName, sound => {
             lastSound = sound;
         });
 
-        events.on(recoverEventName, () => {
+        state.events.on(recoverEventName, () => {
             soundRecovered = true;
             if (typeof recoverCallback === 'function') {
                 recoverCallback(lastSound);
             }
         });
 
-        while (!soundRecovered && instructionPointer < instructions.length && instructionPointer >= 0) {
-            instructionPointer = instructions[instructionPointer].operation(registers, instructionPointer);
+        while (!soundRecovered && state.instructionPointer < state.instructions.length && state.instructionPointer >= 0) {
+            state.instructions[state.instructionPointer].Operation(state);
         }
     }
 };
@@ -47,110 +57,218 @@ class Register {
     }
 }
 
+class ProgramState {
+    constructor() {
+        /** @type {Register[]} */
+        this.registers = [];
+
+        /** @type {Instruction[]} */
+        this.instructions = [];
+
+        this.instructionPointer = 0;
+
+        this.events = new EventEmitter();
+    }
+
+    /**
+     * @param {string} name
+     * @returns {Register}
+     */
+    GetRegister(name) {
+        if (this.registers.filter(r => r.name === name).length > 0) {
+            return this.registers.find(r => r.name === name);
+        } else {
+            const r = new Register(name, 0);
+            this.registers.push(r);
+            return r;
+        }
+    }
+
+    /**
+     * @param {string} value
+     * @returns {number}
+     */
+    GetValueOrRegisterValue(value) {
+        if (isNaN(parseInt(value))) {
+            return this.GetRegister(value).value;
+        } else {
+            return parseInt(value);
+        }
+    }
+}
+
 class Instruction {
     /**
-     * @param {(registers: Register[], instructionPointer: number) => number} operation
+     * @param {string[]} params
      */
-    constructor(operation) {
-        /**
-         * @property {(registers: Register[], instructionPointer: number) => number} operation
-         * @readonly
-         * */
-        this.operation = operation;
+    constructor(params) {
+        this.params = params;
+    }
+
+    /**
+     * @param {ProgramState} state
+     */
+    Operation(state) {}
+}
+
+class SoundInstruction extends Instruction {
+    /**
+     * @param {string[]} params
+     */
+    constructor(params) {
+        super(params);
+    }
+
+    /**
+     * @param {ProgramState} state
+     */
+    Operation(state) {
+        state.events.emit(soundEventName, state.GetValueOrRegisterValue(this.params[0]));
+        state.instructionPointer++;
+    }
+}
+
+class SetInstruction extends Instruction {
+    /**
+     * @param {string[]} params
+     */
+    constructor(params) {
+        super(params);
+    }
+
+    /**
+     * @param {ProgramState} state
+     */
+    Operation(state) {
+        const register = state.GetRegister(this.params[0]);
+        register.value = state.GetValueOrRegisterValue(this.params[1]);
+        state.instructionPointer++;
+    }
+}
+
+class AddInstruction extends Instruction {
+    /**
+     * @param {string[]} params
+     */
+    constructor(params) {
+        super(params);
+    }
+
+    /**
+     * @param {ProgramState} state
+     */
+    Operation(state) {
+        const register = state.GetRegister(this.params[0]);
+        register.value += state.GetValueOrRegisterValue(this.params[1]);
+        state.instructionPointer++;
+    }
+}
+
+class MultiplyInstruction extends Instruction {
+    /**
+     * @param {string[]} params
+     */
+    constructor(params) {
+        super(params);
+    }
+
+    /**
+     * @param {ProgramState} state
+     */
+    Operation(state) {
+        const register = state.GetRegister(this.params[0]);
+        register.value *= state.GetValueOrRegisterValue(this.params[1]);
+        state.instructionPointer++;
+    }
+}
+
+class ModulusInstruction extends Instruction {
+    /**
+     * @param {string[]} params
+     */
+    constructor(params) {
+        super(params);
+    }
+
+    /**
+     * @param {ProgramState} state
+     */
+    Operation(state) {
+        const register = state.GetRegister(this.params[0]);
+        register.value %= state.GetValueOrRegisterValue(this.params[1]);
+        state.instructionPointer++;
+    }
+}
+
+class RecoverInstruction extends Instruction {
+    /**
+     * @param {string[]} params
+     */
+    constructor(params) {
+        super(params);
+    }
+
+    /**
+     * @param {ProgramState} state
+     */
+    Operation(state) {
+        const checkValue = state.GetValueOrRegisterValue(this.params[0]);
+
+        if (checkValue !== 0) {
+            state.events.emit(recoverEventName);
+        }
+
+        state.instructionPointer++;
+    }
+}
+
+class JumpGreaterThanZeroInstruction extends Instruction {
+    /**
+     * @param {string[]} params
+     */
+    constructor(params) {
+        super(params);
+    }
+
+    /**
+     * @param {ProgramState} state
+     */
+    Operation(state) {
+        const checkValue = state.GetValueOrRegisterValue(this.params[0]);
+        const offset = state.GetValueOrRegisterValue(this.params[1]);
+
+        if (checkValue > 0) {
+            state.instructionPointer += offset;
+        } else {
+            state.instructionPointer++;
+        }
     }
 }
 
 /**
  * @param {string} input
+ * @param {InstructionMapping} instructionMapping
  * @returns {Instruction[]}
  */
-function parse(input) {
+function parse(input, instructionMapping) {
     /** @type {Instruction[]} */
     const instructions = [];
     input.split(/\n/).forEach(line => {
         const matches = line.match(/(\w{3})\s(-?\d+|\w)(\s(-?\d+|\w))?/);
-        const instr = matches[1];
-
-        if (instr === 'snd') {
-            instructions.push(new Instruction((registers, instructionPointer) => {
-                events.emit(soundEventName, getValueOrRegisterValue(matches[2], registers))
-                return instructionPointer + 1;
-            }));
-        } else if (instr === 'set') {
-            instructions.push(new Instruction((registers, instructionPointer) => {
-                const register = getRegister(matches[2], registers);
-                register.value = getValueOrRegisterValue(matches[4], registers);
-                return instructionPointer + 1;
-            }));
-        } else if (instr === 'add') {
-            instructions.push(new Instruction((registers, instructionPointer) => {
-                const register = getRegister(matches[2], registers);
-                register.value += getValueOrRegisterValue(matches[4], registers);
-                return instructionPointer + 1;
-            }));
-        } else if (instr === 'mul') {
-            instructions.push(new Instruction((registers, instructionPointer) => {
-                const register = getRegister(matches[2], registers);
-                register.value *= getValueOrRegisterValue(matches[4], registers);
-                return instructionPointer + 1;
-            }));
-        } else if (instr === 'mod') {
-            instructions.push(new Instruction((registers, instructionPointer) => {
-                const register = getRegister(matches[2], registers);
-                register.value %= getValueOrRegisterValue(matches[4], registers);
-                return instructionPointer + 1;
-            }));
-        } else if (instr === 'rcv') {
-            instructions.push(new Instruction((registers, instructionPointer) => {
-                const checkValue = getValueOrRegisterValue(matches[2], registers);
-
-                if (checkValue !== 0) {
-                    events.emit(recoverEventName);
-                }
-
-                return instructionPointer + 1;
-            }));
-        } else if (instr === 'jgz') {
-            instructions.push(new Instruction((registers, instructionPointer) => {
-                const checkValue = getValueOrRegisterValue(matches[2], registers);
-                const offset = getValueOrRegisterValue(matches[4], registers);
-
-                if (checkValue > 0) {
-                    return instructionPointer + offset;
-                } else {
-                    return instructionPointer + 1;
-                }
-            }));
-        }
+        instructions.push(new instructionMapping[matches[1]]([matches[2], matches[4]]));
     });
 
     return instructions;
 }
 
-/**
- * @param {string} value
- * @param {Register[]} registers
- * @returns {number}
- */
-function getValueOrRegisterValue(value, registers) {
-    if (isNaN(parseInt(value))) {
-        return getRegister(value, registers).value;
-    } else {
-        return parseInt(value);
-    }
-}
-
-/**
- * @param {string} name
- * @param {Register[]} registers
- * @returns {Register}
- */
-function getRegister(name, registers) {
-    if (registers.filter(r => r.name === name).length > 0) {
-        return registers.find(r => r.name === name);
-    } else {
-        const r = new Register(name, 0);
-        registers.push(r);
-        return r;
-    }
+module.exports = {
+    Interpretter,
+    Instruction,
+    SoundInstruction,
+    SetInstruction,
+    AddInstruction,
+    MultiplyInstruction,
+    ModulusInstruction,
+    RecoverInstruction,
+    JumpGreaterThanZeroInstruction
 }
--- solutions/day-18/nodejs/puzzle-1.js
+++ solutions/day-18/nodejs/puzzle-1.js
@@ -3,10 +3,31 @@
 
 const fs = require('fs');
 const path = require('path');
-const { Interpretter } = require('./interpretter');
+const {
+    Interpretter,
+    SoundInstruction,
+    SetInstruction,
+    AddInstruction,
+    MultiplyInstruction,
+    ModulusInstruction,
+    RecoverInstruction,
+    JumpGreaterThanZeroInstruction
+} = require('./interpretter');
 
 const input = fs.readFileSync(path.resolve(__dirname, '../input.txt'), 'utf-8');
 
-Interpretter.Execute(input, sound => {
-    console.log(sound);
-});
+Interpretter.Execute(
+    input,
+    {
+        snd: SoundInstruction,
+        set: SetInstruction,
+        add: AddInstruction,
+        mul: MultiplyInstruction,
+        mod: ModulusInstruction,
+        rcv: RecoverInstruction,
+        jgz: JumpGreaterThanZeroInstruction
+    },
+    sound => {
+        console.log(sound);
+    }
+);
